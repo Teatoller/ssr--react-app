778f30cb926ea710b4a9c99daac6087b55ac7381
stevenennis <steven.otieno1@gmail.com>
2023-04-12 11:50:09 +0300

    ft(pages): add pages component with navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Articles from './components/Articles';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-     <h1>Hello</h1>
+     <h1>Server Side Rendering Example</h1>
     </div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/articles">Articles</Link></li>
+    </ul>
+    <Routes>
+    <Route exact path="/" element={<Home />} />
+      
+      <Route path="about/*" element={<About />} />
+      <Route path="articles/*" element={<Articles />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Articles.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Articles() {
+  return (
+    <div className="App"><h1>Articles</h1></div>
+  )
+}
